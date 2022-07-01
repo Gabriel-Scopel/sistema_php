f82eeaa2ddcfc291be1d94b97f11e7b4b1bcfd67
Gabriel-Scopel <gabriel.souza.scopel@hotmail.com>
2022-06-30 22:06:20 -0300

    inicio/adicionandoUsuario

--- adicionar.php
+++ adicionar.php
@@ -1,4 +1,14 @@
+<h1>Adicionar Usuário</h1>
 <form method="post" action="adicionar_action.php">
-    ...
+    <label>
+        Nome:<br/>
+        <input type="text" name="name"/>
+    </label><br/><br/>
 
+    <label>
+        Email:<br/>
+        <input type="email" name="email"/>
+    </label><br/><br/>
+
+    <input type="submit" value="Adicionar"/>
 </form>
